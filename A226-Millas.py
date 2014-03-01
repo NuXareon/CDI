54cc0f20cfa712e8c706b895743828e4e383ea6d
NuXareon <millas@gmail.com>
2014-03-01 14:21:02 +0100

    Createt A226 (not finished)
    1/2 Graph done

--- A226-Millas.py
+++ A226-Millas.py
@@ -42,7 +42,27 @@ plt.annotate('$-\log_2(p)$',xy=(p,-log2(p)), xytext=(-0.75,q-0.05), fontsize=16)
 
 plt.show()
 
+##############
 
+x = np.arange(0.001,1.001,0.01)
 
+fig = plt.figure(figsize=(10,6))
 
+plt.xlim(-0.1,1.2)
+plt.ylim(-0.1,0.7)
 
+plt.plot(x, -x*log2(x), lw=3, color='black')
+
+hline(0,1.05,0)
+vline(0,0.65,0)
+
+p = 1/e
+
+vline(0,-p*log2(p),p,dash='k--')
+hline(0,0.5,-p*log2(p),dash='k--')
+
+plt.annotate('$-p*\log_2(p)$',xy=(0.8,0.4), xytext=(0.8,0.4), fontsize=16)
+plt.annotate('$\log_2(e)/e$ =~ $0.53$',xy=(0.4,0.2), xytext=(0.4,0.2), fontsize=16)
+plt.annotate('$1/e$',xy=(p,0), xytext=(0.34,-0.05), fontsize=16)
+
+plt.show()
