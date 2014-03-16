93e89b8e10c39c0e79543902d4b1126d543e13a0
NuXareon <millas@gmail.com>
2014-03-16 17:21:57 +0100

    A312

--- A312-Millas.py
+++ A312-Millas.py
@@ -1,7 +1,8 @@
 # A312x: kick off file for A312
 
-#pypath="d:/Docencia/2014/py/"
-pypath="/home/nuxe/pyzo2013c/CDI"
+pypath="d:/Docencia/2014/py/"
+#pypath="/home/nuxe/pyzo2013c/CDI"
+#pypath="C:/Users/nuXe/Documents/GitHub/CDI"
 
 # Add pypath to path
 import sys
@@ -9,6 +10,7 @@ sys.path.append(pypath)
 
 # Data from the Morse code, and utilities
 from A312h import *
+from A305_EN1 import *
 
 # Compute the weighted average (mean) length of the Morse codes (without space)
 # Write expressiona that yield the list of those lengths in the alphabetical order
@@ -34,3 +36,16 @@ print(round(ell,3))
 
 sigma2 = sum([P[j] * (L[j]-ell)**2 for j in R])
 print(round(sigma2,3))
+
+ell_2 = sum(ME_[x][0]*ME_[x][1] for x in AZ)
+sigma2_2 = sum(ME_[x][0]*(ME_[x][1]-ell_2)**2 for x in AZ)
+
+e1_=entropy(EN_.values())
+
+print("Mean lenght l =",ell_2)
+print("Entropy of EN_ =",e1_)
+print("By coding each letter of the english alphabet with a diferent number of bits depending of their probability we have reduced the entropy, that means that we need a lower amount of bits to codify an english text thanks to the new codifycation.")
+print("Comparing morse and the new encoding:")
+print("Mean lenght: morse = ", ell, ", codification = ", ell_2)
+print("Variance: morse = ",sigma2, ", codification = ",sigma2_2)
+print("We can see from this values that the morse code uses less bits to codify an english text. However, we have to take into acount that the morse code is more time consuming since it uses \"silences\" to represent the end of a character or the space")
--- A312h.py
+++ A312h.py
@@ -1,5 +1,6 @@
 # A312h: data and utilities for A312
 
+from math import log
 
 # Frequencies of characters
 F={'a': 651738, 'b': 124248, 'c': 217339, 'd': 349835, 'e':1041442, 'f': 197881, 'g': 158610, 
@@ -17,6 +18,16 @@ M = {
 'y':(1,3), 'z':(2,2)
 }
 
+ME_ = {
+'a':(0.0545,4), 'b':(0.0128,6), 'c':(0.0263,5), 'd':(0.0285,5), 
+'e':(0.0913,4), 'f':(0.0173,6), 'g':(0.0133,5), 'h':(0.0313,5),
+'i':(0.0599,4), 'j':(0.0006,10), 'k':(0.0084,7), 'l':(0.0335,5),
+'m':(0.0235,6), 'n':(0.0596,4), 'o':(0.0689,4), 'p':(0.0192,6),
+'q':(0.0008,9), 'r':(0.0508,5), 's':(0.0567,4), 't':(0.0706,4),
+'u':(0.0334,5), 'v':(0.0069,8), 'w':(0.0119,7), 'x':(0.0073,7),
+'y':(0.0164,6), 'z':(0.0007,10), '_':(0.1928,2)
+}
+
 
 def is_sequence(arg):
     return (not hasattr(arg, "strip") and
@@ -29,3 +40,16 @@ def round(f,n):
     if is_sequence(f):
         return [round(x,n) for x in f]
 
+def log2(x): return log(x)/log(2)
+
+def entropy(W):
+    S = sum(W)
+    #assert S != 0
+    if S == 0: return 'A305h/entropy(W): W is not a weight distribution' 
+    H = 0
+    for w in W:
+        if w < 0: return 'A305h/entropy(W): W is not a weight distribution'
+        if w == 0: continue
+        else: H -= w * log2(w)
+    H += S * log2(S)
+    return H/S
